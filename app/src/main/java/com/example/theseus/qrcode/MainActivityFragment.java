e810107e731ff5c526dec6397fdf6e18985c8252
theseus007 <2013001@iiitdmj.ac.in>
2017-04-14 11:59:20 +0530

    feat:Add activity to show QR code

--- app/src/main/java/com/example/theseus/qrcode/MainActivityFragment.java
+++ app/src/main/java/com/example/theseus/qrcode/MainActivityFragment.java
@@ -1,10 +1,21 @@
 package com.example.theseus.qrcode;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -13,10 +24,31 @@ public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
     }
-
+    Button generate_QRCode;
+    ImageView qrCode;
+    EditText mEditText;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view= inflater.inflate(R.layout.fragment_main, container, false);
+        generate_QRCode=(Button)view.findViewById(R.id.generate_qr);
+        qrCode=(ImageView)view.findViewById(R.id.imageView);
+        mEditText=(EditText)view.findViewById(R.id.editText);
+        generate_QRCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text=mEditText.getText().toString();
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                try {
+                    BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                    qrCode.setImageBitmap(bitmap);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return view;
     }
 }
